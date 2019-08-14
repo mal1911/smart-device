07fbb0861abce5511616023a323c00b6f6bd1a14
mal1911 <ma1911@mail.ru>
2019-08-14 09:22:22 +0300

    Замечания исправлены

--- build/js/main.js
+++ build/js/main.js
@@ -25,12 +25,19 @@
   var modalFormElement = document.querySelector('.modal__form');
   var modalCloseElement = document.querySelector('.modal__close');
   var headerButtonElement = document.querySelector('.header__button');
+  var scrollUpButton = document.querySelector('.motivation__up');
+
 
   var onKeyPress = function (evt) {
     if (evt.keyCode === ESC_KEYCODE) {
       closeModal();
       evt.preventDefault();
     }
+  };
+
+  var onOverlayClick = function (evt) {
+    closeModal();
+    evt.preventDefault();
   };
 
   var openModal = function () {
@@ -63,7 +70,37 @@
     //
   };
 
+  var onScrollUpButtonClick = function () {
+    document.querySelector('.advantages').scrollIntoView();
+  };
+
+  var onResize = function () {
+    var elementAboutText = document.querySelector('.about__text--last');
+    var aboutText = document.querySelector('.about__text--last span').textContent;
+    var visibleElement = document.querySelector('.about__text--last span:nth-child(2)');
+
+    if (!visibleElement) {
+      visibleElement = document.createElement('span');
+      elementAboutText.insertAdjacentElement('beforeend', visibleElement);
+    }
+
+    if (screen.width < 768) {
+      visibleElement.textContent = aboutText.slice(0, 237) + '..';
+    } else {
+      visibleElement.textContent = aboutText;
+    }
+  };
+
+  var main = function () {
+    onResize();
+  };
+
   modalFormElement.addEventListener('submit', onFormSubmit);
   modalCloseElement.addEventListener('click', onModalCloseClick);
   headerButtonElement.addEventListener('click', onHeaderButtonClick);
+  overlayElement.addEventListener('click', onOverlayClick);
+  scrollUpButton.addEventListener('click', onScrollUpButtonClick);
+  window.addEventListener('resize', onResize);
+
+  main();
 })();
